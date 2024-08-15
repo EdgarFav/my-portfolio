c0662f298898317567a84d348330ee35e086176b
EdgarFav <edgarfda17@gmail.com>
2024-08-15 14:02:55 -0600

    Feat: conditional render tabs to show dinamic info in About Section

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -2,9 +2,10 @@
 import React, { useState, useTransition } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton'
+import { TAB_DATA } from '../constants'
 
 const AboutSection = () => {
-    const [tab, setTab] = useState("skills")
+    const [tab, setTab] = useState("Skills")
     const [isPending, startTransition] = useTransition()
 
     const handleTabChange = (id) => {
@@ -12,11 +13,14 @@ const AboutSection = () => {
             setTab(id)
         })
     }
+
+    const tabData = TAB_DATA.find((elem) => elem.id === tab)
+
     return (
         <section className='text-white'>
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
                 <Image src='/dragon.jpg' alt='About me' width={500} height={500} />
-                <div>
+                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold text-white mb-4'>About me</h2>
                     <p className='text-base lg:text-lg'>
                         I am a full-stack developer with a passion for creating
@@ -25,23 +29,24 @@ const AboutSection = () => {
                         MongoDB, and I am always looking for new challenges and
                         opportunities to grow my skills and knowledge.
                     </p>
-                    <div className='flex flex-row mt-8'>
+                    <div className='flex flex-row justify-start mt-8'>
                         <TabButton
                             selectTab={() => handleTabChange("Skills")}
                             active={tab === "Skills"}>
                             {" "}Skills{" "}
                         </TabButton>
                         <TabButton
-                            selectTab={() => handleTabChange("Experience")}
-                            active={tab === "Experience"}>
-                            {" "}Experience{" "}
-                        </TabButton>
-                        <TabButton
                             selectTab={() => handleTabChange("Education")}
                             active={tab === "Education"}>
                             {" "}Education{" "}
                         </TabButton>
+                        <TabButton
+                            selectTab={() => handleTabChange("Certifications")}
+                            active={tab === "Certifications"}>
+                            {" "}Certifications{" "}
+                        </TabButton>
                     </div>
+                    <div className='mt-8'>{tabData.content}</div>
                 </div>
             </div>
         </section>
--- app/constants/index.tsx
+++ app/constants/index.tsx
@@ -21,3 +21,33 @@ export const NavLinks = [
         title: "Contact",
     },
 ];
+
+export const TAB_DATA = [
+    {title: "Skills", id: "Skills", content: (
+        <ul className='list-disc pl-2'>
+        <li>javascript</li>
+        <li>react</li>
+        <li>nodejs</li>
+        <li>mongodb</li>
+        <li>express</li>
+        <li>typescript</li>
+        <li>graphql</li>
+        <li>nextjs</li>
+        <li>tailwindcss</li>
+        <li>sass</li>
+        <li>bootstrap</li>
+        </ul>
+    )},
+{title: "Education", id: "Education", content: (
+        <ul className='list-disc pl-2'>
+        <li>Master of Computer Science</li>
+        <li>Bachelor of Engineering</li>
+        </ul>
+    )},
+    {title: "Certifications", id: "Certifications", content: (
+        <ul className='list-disc pl-2'>
+        <li>Google Analytics Certified</li>
+        <li>Google Tag Manager Certified</li>
+        </ul>
+    )},
+]
